57fe0005d2ad6b3788a1cacf4f01df69ca0ff16e
Péter Megyesi <megyesi@tmit.bme.hu>
2024-12-19 10:40:38 +0100

    Add README

--- install-nodes.sh
+++ install-nodes.sh
@@ -7,16 +7,11 @@ do
     scp -o "StrictHostKeyChecking no" manifests/*  root@$ip:
     ssh -o "StrictHostKeyChecking no" root@$ip /root/local-install.sh
     
+    # You can use these lines if you want to run a specific command an all VMs
     #ssh -o "StrictHostKeyChecking no" root@$ip kubectl apply -f /root/stunner-common.yaml
     #ssh -o "StrictHostKeyChecking no" root@$ip helm repo update
     #ssh -o "StrictHostKeyChecking no" root@$ip helm upgrade stunner-gateway-operator stunner/stunner-gateway-operator --namespace=stunner
 
-    #ssh -o "StrictHostKeyChecking no" root@$ip REGION=$(echo $HOSTNAME | cut -d'-' -f5) sed -i "s/MYREGION/$REGION/g" livekit.yaml
-    #ssh -o "StrictHostKeyChecking no" root@$ip kubectl apply -f /root/livekit.yaml -n livekit
-    #ssh -o "StrictHostKeyChecking no" root@$ip kubectl delete pod -n livekit -l app=lk-meet
-    #ssh -o "StrictHostKeyChecking no" root@$ip kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.1/cert-manager.yaml
-	
-	#coturn install
-	#ssh -o "StrictHostKeyChecking no" root@$ip sed -i "s/99.99.99.99/$ip/g" coturn.yaml
-	#ssh -o "StrictHostKeyChecking no" root@$ip kubectl apply -n stunner -f coturn.yaml
+    #ssh -o "StrictHostKeyChecking no" root@$ip REGION=$(hostname | cut -d"-" -f4,5) sed -i "s/MYREGION/$REGION/g" broadcaster.yaml
+    #ssh -o "StrictHostKeyChecking no" root@$ip kubectl apply -f /root/broadcaster.yaml
 done
