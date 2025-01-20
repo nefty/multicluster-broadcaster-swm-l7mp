bd7870f8777fd0ab3fb6c4bdd0d0feb3b49afef2
Michał Śledź <michalsledz34@gmail.com>
2025-01-20 18:56:55 +0100

    Add first version of stats recorder and comparator

--- k8s_broadcaster/assets/js/home.js
+++ k8s_broadcaster/assets/js/home.js
@@ -111,6 +111,7 @@ async function connectInput(view) {
     view.whepClient.changeLayer(view.defaultLayer);
 
     if (view.whepClient.pc.connectionState === "connected") {
+      view.recordBtn.onclick = () => startRecording(view);
       view.stats.startTime = new Date();
       view.stats.intervalId = setInterval(readPCStats, 1000, view);
     }
@@ -187,6 +188,7 @@ function processCandidatePairReport(view, report) {
   view.stats.rtt.innerText = report.currentRoundTripTime * 1000;
   view.stats.rttShort.innerText = report.currentRoundTripTime * 1000;
   view.stats.rttTS.push(timestamp, report.currentRoundTripTime * 1000);
+  record(view.recorder.rttTS, timestamp, report.currentRoundTripTime * 1000);
   view.rttChart.update();
 
   let packetsReceived = 0;
@@ -207,6 +209,8 @@ function processCandidatePairReport(view, report) {
 
   view.stats.packetsReceived.innerText = packetsReceived;
   view.stats.packetsReceivedTS.push(timestamp, packetsReceived);
+  record(view.recorder.packetsReceivedTS, timestamp, packetsReceived);
+
   view.packetsReceivedChart.update();
 
   view.stats.lastCandidatePairReport = report;
@@ -283,6 +287,20 @@ function processVideoReport(view, report) {
   view.stats.freezeCountTS.push(timestamp, report.freezeCount);
   view.stats.freezeDurationTS.push(timestamp, report.totalFreezesDuration);
   view.stats.nackCountTS.push(timestamp, report.nackCount);
+
+  record(view.recorder.videoBitrateTS, timestamp, btr);
+  record(view.recorder.fpsTS, timestamp, report.framesPerSecond || 0);
+  record(view.recorder.keyframesDecodedTS, timestamp, report.keyFramesDecoded);
+  record(view.recorder.pliCountTS, timestamp, report.pliCount);
+  record(view.recorder.avgJitterTS, timestamp, avgJitterBufferDelay.toFixed(2));
+  record(view.recorder.freezeCountTS, timestamp, report.freezeCount);
+  record(
+    view.recorder.freezeDurationTS,
+    timestamp,
+    report.totalFreezesDuration
+  );
+  record(view.recorder.nackCountTS, timestamp, report.nackCount);
+
   view.videoChart.update();
   view.fpsChart.update();
   view.keyframesDecodedChart.update();
@@ -324,6 +342,7 @@ function processAudioReport(view, report) {
   view.stats.lastAudioReport = report;
 
   view.stats.audioBitrateTS.push(timestamp, btr);
+  record(view.recorder.audioBitrateTS, timestamp, btr);
   view.audioChart.update();
 }
 
@@ -378,6 +397,7 @@ function updatePacketLoss(view) {
   view.stats.packetLoss.innerText = packetLoss;
 
   view.stats.packetLossTS.push(timestamp, packetLoss);
+  record(view.recorder.packetLossTS, timestamp, packetLoss);
   view.packetLossChart.update();
 }
 
@@ -397,21 +417,14 @@ function toHHMMSS(milliseconds) {
   return hours + ":" + minutes + ":" + seconds;
 }
 
-function resetButtons(view) {
-  view.button1.classList.remove("border-green-500");
-  view.button2.classList.remove("border-green-500");
-  view.button3.classList.remove("border-green-500");
-  view.buttonAuto.classList.remove("border-green-500");
-}
-
-function colorButton(button) {
-  button.classList.toggle("border-green-500");
+function record(ts, timestamp, data) {
+  if (ts) {
+    ts.push(timestamp, data);
+  }
 }
 
 function buttonOnClick(view, button) {
   view.url = button.value;
-  resetButtons(view);
-  colorButton(button);
   view.globe.setConnectedRegion(null);
   connectInput(view);
 }
@@ -463,6 +476,267 @@ function createChart(ctx, name, ts) {
   });
 }
 
+function createCompChart(ctx, name, ts1, ts2) {
+  return new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: ts1.x,
+      datasets: [
+        {
+          data: ts1.y,
+          borderWidth: 1,
+          pointRadius: 0,
+          borderColor: "#675AF1",
+        },
+        {
+          data: ts2.y,
+          borderWidth: 1,
+          pointRadius: 0,
+          borderColor: "#3AC2BE",
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: name,
+        },
+        legend: {
+          display: false,
+        },
+      },
+      scales: {
+        y: {
+          beginAtZero: false,
+          min: 0,
+        },
+        x: {
+          ticks: {
+            maxRotation: 0,
+          },
+        },
+      },
+    },
+  });
+}
+
+function destroyChart(chart) {
+  if (chart) {
+    chart.destroy();
+  }
+}
+
+// recording related functions
+function startRecording(view) {
+  // one hour assuming getStats is called every second
+  maxPoints = 3600;
+  view.recorder = {
+    rttTS: new TimeSeries(maxPoints),
+    audioBitrateTS: new TimeSeries(maxPoints),
+    videoBitrateTS: new TimeSeries(maxPoints),
+    packetsReceivedTS: new TimeSeries(maxPoints),
+    fpsTS: new TimeSeries(maxPoints),
+    keyframesDecodedTS: new TimeSeries(maxPoints),
+    pliCountTS: new TimeSeries(maxPoints),
+    packetLossTS: new TimeSeries(maxPoints),
+    nackCountTS: new TimeSeries(maxPoints),
+    avgJitterTS: new TimeSeries(maxPoints),
+    freezeCountTS: new TimeSeries(maxPoints),
+    freezeDurationTS: new TimeSeries(maxPoints),
+  };
+
+  view.record.onclick = () => stopRecording(view);
+  view.record.innerText = "Stop recording";
+}
+
+function stopRecording(view) {
+  const rows = zip(
+    view.recorder.rttTS.y,
+    view.recorder.audioBitrateTS.y,
+    view.recorder.videoBitrateTS.y,
+    view.recorder.packetsReceivedTS.y,
+    view.recorder.fpsTS.y,
+    view.recorder.keyframesDecodedTS.y,
+    view.recorder.pliCountTS.y,
+    view.recorder.packetLossTS.y,
+    view.recorder.nackCountTS.y,
+    view.recorder.avgJitterTS.y,
+    view.recorder.freezeCountTS.y,
+    view.recorder.freezeDurationTS.y
+  );
+
+  let csvContent =
+    "rtt,audioBitrate,videoBitrate,packetsReceived,fps,keyframesDecoded,pliCount,packetLoss,nackCount,avgJitter,freezeCount,freezeDuration\n" +
+    rows.map((r) => r.join(",")).join("\n");
+
+  var file = new Blob([csvContent], { type: "text" });
+
+  var a = document.createElement("a");
+  var url = URL.createObjectURL(file);
+  a.href = url;
+  a.download = "stats.csv";
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(function () {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 0);
+
+  view.recorder = {};
+
+  view.record.onclick = () => startRecording(view);
+  view.record.innerText = "Record";
+}
+
+function zip(...arrays) {
+  return arrays[0].map(function (_e, i) {
+    return arrays.map(function (a) {
+      return a[i];
+    });
+  });
+}
+
+function parseCsv(content) {
+  const rowsTxt = content.split("\n");
+  const rows = rowsTxt.map(function (r) {
+    return r.split(",");
+  });
+  return zip(...rows.slice(1));
+}
+
+function compare(view) {
+  const f1 = view.file1.files[0];
+  const f2 = view.file2.files[0];
+
+  if (!f1 || !f2) return;
+
+  const reader1 = new FileReader();
+  const reader2 = new FileReader();
+
+  var columns1;
+  var columns2;
+
+  reader1.onload = (ev) => (columns1 = parseCsv(ev.target.result));
+  reader2.onload = (ev) => {
+    columns2 = parseCsv(ev.target.result);
+    displayComparision(view, columns1, columns2);
+  };
+
+  reader1.readAsText(f1);
+  reader2.readAsText(f2);
+}
+
+function displayComparision(view, columns1, columns2) {
+  let ts1 = columns1.map(function (c) {
+    return c.reduce(function (ts, el, idx) {
+      ts.push(idx, el);
+      return ts;
+    }, new TimeSeries(3600));
+  });
+
+  let ts2 = columns2.map(function (c) {
+    return c.reduce(function (ts, el, idx) {
+      ts.push(idx, el);
+      return ts;
+    }, new TimeSeries(3600));
+  });
+
+  if (ts2[0].x.length > ts1[0].x.length) {
+    // switch time series
+    // we always draw t1.x.length number of points
+    const tmp = ts1;
+    ts1 = ts2;
+    ts2 = tmp;
+  }
+
+  destroyChart(view.crttChart);
+  destroyChart(view.ccaudioChart);
+  destroyChart(view.cvideoChart);
+  destroyChart(view.cpacketsReceivedChart);
+  destroyChart(view.cfpsChart);
+  destroyChart(view.ckeyframesDecodedChart);
+  destroyChart(view.cpliCountChart);
+  destroyChart(view.cpacketLossChart);
+  destroyChart(view.cnackCountChart);
+  destroyChart(view.cavgJitterChart);
+  destroyChart(view.cfreezeCountChart);
+  destroyChart(view.cfreezeDurationChart);
+
+  view.crttChart = createCompChart(
+    document.getElementById("c-rtt-chart"),
+    "RTT (ms)",
+    ts1[0],
+    ts2[0]
+  );
+  view.ccaudioChart = createCompChart(
+    document.getElementById("c-audio-chart"),
+    "audioBitrate (kbps)",
+    ts1[1],
+    ts2[1]
+  );
+  view.cvideoChart = createCompChart(
+    document.getElementById("c-video-chart"),
+    "videoBitrate (kbps)",
+    ts1[2],
+    ts2[2]
+  );
+  view.cpacketsReceivedChart = createCompChart(
+    document.getElementById("c-packetsReceived-chart"),
+    "packetsReceived/s",
+    ts1[3],
+    ts2[3]
+  );
+  view.cfpsChart = createCompChart(
+    document.getElementById("c-fps-chart"),
+    "FPS",
+    ts1[4],
+    ts2[4]
+  );
+  view.ckeyframesDecodedChart = createCompChart(
+    document.getElementById("c-keyframesDecoded-chart"),
+    "keyframesDecoded",
+    ts1[5],
+    ts2[5]
+  );
+  view.cpliCountChart = createCompChart(
+    document.getElementById("c-pliCount-chart"),
+    "pliCount",
+    ts1[6],
+    ts2[6]
+  );
+  view.cpacketLossChart = createCompChart(
+    document.getElementById("c-packetLoss-chart"),
+    "packetLoss (%)",
+    ts1[7],
+    ts2[7]
+  );
+  view.cnackCountChart = createCompChart(
+    document.getElementById("c-nackCount-chart"),
+    "nackCount",
+    ts1[8],
+    ts2[8]
+  );
+  view.cavgJitterChart = createCompChart(
+    document.getElementById("c-avgJitter-chart"),
+    "avgJitterBufferDelay (ms)",
+    ts1[9],
+    ts2[9]
+  );
+  view.cfreezeCountChart = createCompChart(
+    document.getElementById("c-freezeCount-chart"),
+    "freezeCount",
+    ts1[10],
+    ts2[10]
+  );
+  view.cfreezeDurationChart = createCompChart(
+    document.getElementById("c-freezeDuration-chart"),
+    "freezeDuration",
+    ts1[11],
+    ts2[11]
+  );
+}
+
 export const Home = {
   mounted() {
     // read html elements
@@ -482,6 +756,30 @@ export const Home = {
     view.button2 = document.getElementById("button-2");
     view.button3 = document.getElementById("button-3");
     view.buttonAuto = document.getElementById("button-auto");
+    view.recordBtn = document.getElementById("record-btn");
+    view.statisticsTab = document.getElementById("statistics-tab");
+    view.compareTab = document.getElementById("compare-tab");
+    view.compareStats = document.getElementById("compare-stats");
+    view.videoplayerStats = document.getElementById("videoplayer-stats");
+    view.file1 = document.getElementById("file-1");
+    view.file2 = document.getElementById("file-2");
+    view.compareBtn = document.getElementById("compare-btn");
+
+    view.statisticsTab.onclick = () => {
+      view.videoplayerStats.classList.remove("hidden");
+      view.videoplayerStats.classList.add("flex");
+      view.compareStats.classList.remove("hidden");
+      view.compareStats.classList.remove("flex");
+      view.compareStats.classList.add("hidden");
+    };
+
+    view.compareTab.onclick = () => {
+      view.videoplayerStats.classList.remove("hidden");
+      view.videoplayerStats.classList.remove("flex");
+      view.videoplayerStats.classList.add("hidden");
+      view.compareStats.classList.remove("hidden");
+      view.compareStats.classList.add("flex");
+    };
 
     view.stats = {
       duration: document.getElementById("duration"),
@@ -520,6 +818,8 @@ export const Home = {
       freezeDurationTS: new TimeSeries().populateLastMinute(),
     };
 
+    view.recorder = {};
+
     // declare custom fields
     view.whepEndpointBase = `${window.location.origin}/api/whep`;
     view.defaultLayer = "h";
@@ -540,6 +840,8 @@ export const Home = {
     view.button2.onclick = (ev) => buttonOnClick(view, ev.srcElement);
     view.button3.onclick = (ev) => buttonOnClick(view, ev.srcElement);
     view.buttonAuto.onclick = (ev) => buttonOnClick(view, ev.srcElement);
+
+    view.compareBtn.onclick = () => compare(view);
 
     view.rttChart = createChart(
       document.getElementById("rtt-chart"),
